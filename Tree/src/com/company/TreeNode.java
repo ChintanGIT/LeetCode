a059a7506c0a7a7f01365e3616de23d5da7199ff
ChintanGIT <agrawal.chintan2@gmail.com>
2022-02-25 15:51:18 -0800

    as

---
+++ Tree/src/com/company/TreeNode.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.sun.source.tree.Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val ){
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
